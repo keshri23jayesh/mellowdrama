b7a4eb8e03d4d9627f3b0c23ebb8076d5a866211
Vaibhav2110 <vaibhpraky@gmail.com>
2018-03-04 18:40:55 +0530

    some changes

---
+++ index2.php
@@ -0,0 +1,500 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  
+    <link href="css/carousel.css" rel="stylesheet">
+    <link href="css/changes.css" rel="stylesheet">
+   
+
+<style>
+
+.navbar-fixed-top {
+    position: fixed;
+    right: 0;
+    left: 0;
+    z-index: 1030;
+}
+
+ .navbar-fixed-top.scrolled {
+  background-color: #fff !important;
+  box-shadow: 0 0 1px #000000;
+  transition: background-color 300ms linear;
+}
+.navbar-toggler{
+  background-color: transparent;
+}
+.nav-link{
+padding-right: .8rem !important; 
+padding-left: .8rem !important;
+}
+
+.contactus {
+        background-image: url("images/mellow.png");
+        background-repeat: no-repeat;
+        background-size: cover;
+        box-shadow: inset 0 0 0 450px rgba(0, 0, 0, 0.95);
+        background-position: center;
+        min-height: 350px;
+    }
+
+
+.container-fluid
+{
+    padding-right: 0px !important; ;
+    padding-left: 0px !important; ;
+}
+.fixedbg{
+	    background-image: url("https://c.slashgear.com/wp-content/uploads/2017/10/batman-gotham-city-enemy-within-episode-2.jpg");
+	    min-height: 720px;
+	    background-attachment: fixed;
+	    background-repeat: no-repeat;
+	    background-size: cover;
+}
+.smallbg{
+	height: 250px;
+	text-align: center;
+	width: 110%;
+	margin: 0;
+	background-image: url("https://c.slashgear.com/wp-content/uploads/2017/10/batman-gotham-city-enemy-within-episode-2.jpg");
+	
+	
+	
+}
+.smallbg > img{
+	max-width: 100%;
+	width:40%;
+	height:auto;
+	display:block;
+	margin-left: 30%;
+	position: relative;
+	top: 20%;
+	border-radius:50%;
+	z-index: 9999;
+	
+	
+		
+	}
+
+
+</style>
+
+
+
+
+</head>
+  <body>
+     
+
+
+
+<header>
+      <nav class="navbar navbar-toggleable-md navbar-light navbar-fixed-top bg-faded">
+  
+<button class="navbar-toggler" style="float:right !important;" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">Hidden brand</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    </header>
+
+    
+
+      <div id="myCarousel" class="carousel slide" data-ride="carousel" id="startchange">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="first-slide" src="images/slider/1.jpg" alt="First slide">
+      
+          </div>
+          <div class="carousel-item">
+            <img class="first-slide" src="images/slider/4.jpg" alt="First slide">
+    
+          </div>
+          <div class="carousel-item">
+            <img class="first-slide" src="images/slider/5.jpg" alt="First slide">
+ 
+          </div>
+          <div class="carousel-item">
+            <img class="first-slide" src="images/slider/6.jpg" alt="First slide">
+     
+          </div>
+          <div class="carousel-item">
+            <img class="first-slide" src="images/slider/7.jpg" alt="First slide">
+        
+          </div>
+
+          <div class="carousel-item">
+            <img class="first-slide" src="images/slider/8.jpg" alt="First slide">
+        
+          </div>
+          <div class="carousel-item">
+            <img class="first-slide" src="images/slider/9.jpg" alt="First slide">
+        
+          </div>     
+           <div class="carousel-item">
+            <img class="first-slide" src="images/slider/10.jpg" alt="First slide">
+        
+          </div>
+           <div class="carousel-item">
+            <img class="first-slide" src="images/slider/11.jpg" alt="First slide">
+        
+          </div>
+           <div class="carousel-item">
+            <img class="first-slide" src="images/slider/12.jpg" alt="First slide">
+        
+          </div>
+           <div class="carousel-item">
+            <img class="first-slide" src="images/slider/13.jpg" alt="First slide">
+        
+          </div>
+           <div class="carousel-item">
+            <img class="first-slide" src="images/slider/14.jpg" alt="First slide">
+        
+          </div>
+           <div class="carousel-item">
+            <img class="first-slide" src="images/slider/15.jpg" alt="First slide">
+        
+          </div>
+           <div class="carousel-item">
+            <img class="first-slide" src="images/slider/16.jpg" alt="First slide">
+        
+          </div>
+           <div class="carousel-item">
+            <img class="first-slide" src="images/slider/17.jpg" alt="First slide">
+        
+          </div>
+      
+     </div>
+        <!-- <a class="carousel-control-prev" href="#myCar" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCar" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a> -->
+      </div>
+
+      <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      <!-- Our Client Say
+      ================================================== -->
+      <section>
+         
+      <div id="myCar" class="carousel slide hidden-sm-down" style="margin-bottom: 0; data-ride="carousel" id="startchange">
+        <ol class="carousel-indicators">
+          <li data-target="#myCar" data-slide-to="0" class="active"></li>
+          <li data-target="#myCar" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner" style="height:20rem!important ; line-height:20rem !important">
+          <div class="carousel-item active  container">
+            <div class="row"  style="padding-left: 8%; padding:right: 8%; height:20rem !important;">
+           <div class="col">
+           	<img style="height: 50%; width: 50%;vertical-align: middle" src="images/logo/AIIMS_Patna.png">
+           </div>
+           <div class="col">
+           	<img style="height: 50%; width: 50%;vertical-align: middle" src="images/logo/Bihar_Govt.png">
+           </div>
+           <div class="col">
+           	<img style="height: 35%; width: 80%;vertical-align: middle" src="images/logo/DNDI.png">
+           </div>
+           <div class="col">
+           	<img style="height: 50%; width: 50%;vertical-align: middle" src="images/logo/nabard.png">
+           </div>
+          </div>
+          </div>
+          <div class="carousel-item" >
+           <div class="row"  style="padding-left: 8%; padding:right: 8%; height:20rem !important;">
+           <div class="col">
+           	<img style="height: 35%; width: 70%;vertical-align: middle;" src="images/logo/Sammaan.png">
+           </div>
+           <div class="col">
+           	<img style="height: 35%; width: 70%;vertical-align: middle;" src="images/logo/save_the_children_india.png">
+           </div>
+           <div class="col">
+           	<img style="height: 30%; width: 90%;vertical-align: middle;" src="images/logo/WATERAID.png">
+           </div>
+           <div class="col">
+           	<img style="height: 50%; width: 50%;vertical-align: middle;" src="images/logo/woman.png">
+           </div>
+           </div>
+               
+          </div>
+     
+       </div>
+	   
+       <div>
+         <a class="carousel-control-prev" href="#myCar" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCar" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a> 
+      </div>   
+     </div>
+
+    </section>
+
+
+
+      <!-- Our Client Say
+      ================================================== -->
+	  
+	  
+	  
+	  <!-- Our Client Say
+      ================================================== -->
+    <section>
+         
+      <div id="myCar" class="carousel slide hidden-md-up" style="margin-bottom: 0;" data-ride="carousel" id="startchange">
+        <ol class="carousel-indicators">
+          <li data-target="#myCar" data-slide-to="0" class="active"></li>
+          <li data-target="#myCar" data-slide-to="1"></li>
+          <li data-target="#myCar" data-slide-to="2"></li>
+          <li data-target="#myCar" data-slide-to="3"></li>
+          <li data-target="#myCar" data-slide-to="4"></li>
+          <li data-target="#myCar" data-slide-to="5"></li>
+          <li data-target="#myCar" data-slide-to="6"></li>
+          <li data-target="#myCar" data-slide-to="7"></li>
+        </ol>
+        <div class="carousel-inner container" style="height:20rem !important; line-height:20rem !important">
+          <div class="carousel-item active container">
+            <div class="row"  style="padding-left: 8%;text-align: center; width: 100%;padding:right: 8%; height:20rem !important;">
+           <div style="width:100%!important">
+           	<img style="height: 50%; width: 50%;vertical-align: middle;text-align: center" src="images/logo/AIIMS_Patna.png">
+           </div>
+          </div>
+          </div>
+          <div class="carousel-item container" >
+          
+           <div class="row"  style="padding-left: 8%;text-align: center;width:100%; padding:right: 8%; height:20rem !important;">
+          <div style="width:100%!important">
+           	<img style="height: 50%; width: 50%;text-align: center;vertical-align: middle" src="images/logo/Bihar_Govt.png">
+           </div>
+            </div>   
+          </div>
+          <div class="carousel-item container">
+            
+            <div class="row"  style="padding-left: 8%;text-align: center;width:100%; padding:right: 8%; height:20rem !important;">
+          <div style="width:100%!important">
+           	<img style="height: 60%; width: 80%;text-align: center;vertical-align: middle" src="images/logo/DNDI.png">
+           </div>
+</div>
+          </div>
+          <div class="carousel-item container" >
+           
+           <div class="row"  style="padding-left: 8%; text-align: center;width:100%;padding:right: 8%; height:20rem !important;">
+          <div style="width:100%!important">
+           	<img style="height: 50%; width: 50%;text-align: center;vertical-align: middle" src="images/logo/nabard.png">
+           </div>
+             </div>  
+          </div>
+          <div class="carousel-item container" >
+           
+           <div class="row"  style="padding-left: 8%; text-align: center;width:100%;padding:right: 8%; height:20rem !important;">
+          <div style="width:100%!important">
+           	<img style="height: 50%; width: 75%;text-align: center;vertical-align: middle" src="images/logo/Sammaan.png">
+           </div>
+             </div>  
+          </div>
+          <div class="carousel-item container" >
+           
+           <div class="row"  style="padding-left: 8%;text-align: center;width:100%; padding:right: 8%; height:20rem !important;">
+          <div style="width:100%!important">
+           	<img style="height: 50%; width: 75%;text-align: center;vertical-align: middle" src="images/logo/save_the_children_india.png">
+           </div>
+              </div> 
+          </div>
+          <div class="carousel-item container" >
+           
+           <div class="row"  style="padding-left: 8%;text-align: center;width:100%; padding:right: 8%; height:20rem !important;">
+          <div style="width:100%!important">
+           	<img style="height: 50%; width: 75%;text-align: center;vertical-align: middle" src="images/logo/WATERAID.png">
+           </div>
+            </div>   
+          </div>
+          <div class="carousel-item container" >
+           
+           <div class="row"  style="padding-left: 8%;text-align: center; width:100%;padding:right: 8%; height:20rem !important;">
+          <div style="width:100%!important">
+           	<img style="height: 50%; width: 50%;text-align: center;vertical-align: middle" src="images/logo/woman.png">
+           </div>
+           </div>
+               
+          </div>
+       </div>
+	   
+       <div>
+         <a class="carousel-control-prev" href="#myCar" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCar" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a> 
+      </div>   
+     </div>
+
+    </section>
+    
+    
+    <section>
+    	<div class="container hidden-xs-down" style="padding:0; margin:0px; width: 100%">
+    		<div class="row" style="width:100%">
+    			<div class="col-7">
+    				<div class="fixedbg"></div>
+    			</div>
+    			<div class="col-5">
+    			Lorep Ipsum
+    			</div>
+    		</div>
+    	</div>
+    </section>
+    <section>
+    	<div class="container hidden-sm-up" style="padding:0; margin:0px; width: 100%">
+    		<div class="row" style="width:100%">
+    			<div class="col" style="width:100%; padding:0; margin:0px;">
+    				<div class="smallbg">
+    					<img src="https://images-na.ssl-images-amazon.com/images/I/31v3fAMOf3L._SY355_.jpg">
+    				</div>
+    			</div>
+    			
+    		</div>
+    		<div class="row" style="height: 100px; width:100%">
+    			<div class="col" style="width:100%">
+    			Lorep Ipsum
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+
+
+      <!-- Our Client Say
+      ================================================== -->
+	  
+	  
+	  
+    
+
+
+      
+
+      <!-- FOOTER -->
+      <footer class="container-fluid">
+       <div class="container-fluid contactus" id="contact">
+        <br>
+        <center>
+            <h2 style="color:white;">CONTACT US</h2>
+        </center>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-md-3 col-sm-3">
+                        <center>
+                            <img src="address.png" height="50px" width="50px" style="border-radius: 5px;">
+                            <p></p>
+                            <p>ADDRESS</p>
+                            <p>B.I.T patna</p>
+                            <p>Hostel no -2 Room no-350</p>
+                        </center>
+                    </div>
+                    <div class="col-md-3 col-sm-3">
+                        <center>
+                            <img src="call.png" height="50px" width="50px" style="border-radius: 5px;">
+                            <p>PHONES</p>
+                            <p>VISHAL: +91 9931226672</p>
+                            <p>AAKRITI: +91 9504325643</p>
+                        </center>
+                    </div>
+                    <div class="col-md-3 col-sm-3">
+                        <center>
+                            <img src="mail.png" height="50px" width="50px" style="border-radius: 5px;">
+                            <p></p>
+                            <p>CONTACTS</p>
+                            <p>justdeliverit.in@gmail.com</p>
+                        </center>
+                    </div>
+                    <div class="col-md-3 col-sm-3">
+                        <center>
+                            <img src="work_hrs.png" height="50px" width="50px" style="border-radius: 5px;">
+                            <p></p>
+                            <p>WORKING_HRS</p>
+                            <p>Monday-Friday: 9:00 - 18:00</p>
+                            <p>Saturday: 11:00 - 17:00</p>
+                            <p>Sunday: Open</p>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
+       </div>
+<div>
+
+   <center>
+           <p>Copyright ©2018 MelloDrama. All Rights Reserved</p>
+   </center>
+
+</div>
+      </footer>
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+   
+<script>
+       $(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $('.nav-link').css('color', 'black');
+  });
+});
+       </script>
+
+
+
+
+
+  </body>
+</html>
